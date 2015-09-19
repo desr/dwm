03fd2e7d91e11ee502b5ea124fb1c5c96ccf2c9e
David <david@34sp.com>
2015-09-19 09:49:56 +0100

    Add lock command

--- config.h
+++ config.h
@@ -58,6 +58,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *lockcmd[]  = { "slock", NULL }
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,6 +95,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 };
 
 /* button definitions */
